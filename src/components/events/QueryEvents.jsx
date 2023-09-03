8e617644744aaad41f79c2e8bdc894c499d17953
yesilyurtalper <yesilyurtalper@gmail.com>
2023-09-03 22:46:11 +0300

    start to add keyset pagination for event querying

--- src/components/events/QueryEvents.jsx
+++ src/components/events/QueryEvents.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Card, Button, Typography, TextField } from "@mui/material";
 import classes from "./QueryEvents.module.css";
-import { useNavigation, useNavigate, Form } from "react-router-dom";
+import {
+  useNavigation,
+  useNavigate,
+  Form,
+  useActionData,
+} from "react-router-dom";
 
 export default function QueryEvents() {
   const navigation = useNavigation();
   const navigate = useNavigate();
-  const submitting = navigation.state === "submitting";
+  const actionData = useActionData();
+
+  const formRef = useRef();
+  const [directionsDisabled, setDirectionsDisabled] = useState(true);
+
+  useEffect(() => {
+    setDirectionsDisabled(
+      navigation.state === "submitting" || !actionData || !actionData.data
+    );
+  }, [navigation, actionData]);
 
   return (
     <Card className={classes.card}>
@@ -17,30 +31,93 @@ export default function QueryEvents() {
         </Button>
       </div>
 
-      <Form className={classes.form} method="POST">
-        <TextField label="Event Id" name="Id" variant="standard" type="number"/>
-        <TextField label="Owner" name="Author" variant="standard" />
-        <TextField label="Title" name="Title" variant="standard" />
-        <TextField label="Body" name="Body" variant="standard" />
+      <Form
+        className={classes.form}
+        method="POST"
+        ref={formRef}
+        onChange={() => setDirectionsDisabled(true)}
+      >
         <TextField
-          label="Date"
-          name="Date"
-          variant="standard"
-          type="date"
-          InputLabelProps={{ shrink: true }}
-        />
-        <TextField
-          label="Price"
-          name="Price"
+          label="Event Id"
+          name="Id"
           variant="standard"
           type="number"
         />
+        <TextField label="Organizer" name="Author" variant="standard" />
+        <TextField label="Title" name="Title" variant="standard" />
+        <TextField label="Body" name="Body" variant="standard" />
+        <fieldset className={classes.between}>
+          <legend>Date Between</legend>
+          <TextField
+            label="Start"
+            name="DateStart"
+            variant="standard"
+            type="datetime-local"
+            InputLabelProps={{ shrink: true }}
+          />
+          <TextField
+            label="End"
+            name="DateEnd"
+            variant="standard"
+            type="datetime-local"
+            InputLabelProps={{ shrink: true }}
+          />
+        </fieldset>
+
+        <fieldset className={classes.between}>
+          <legend>Price Between</legend>
+          <TextField
+            label="Start"
+            name="PriceStart"
+            variant="standard"
+            type="number"
+            inputProps={{ step: "0.01" }}
+          />
+          <TextField
+            label="End"
+            name="PriceEnd"
+            variant="standard"
+            type="number"
+            inputProps={{ step: "0.01" }}
+          />
+        </fieldset>
+
+        <TextField
+          label="Count"
+          name="Count"
+          variant="standard"
+          type="number"
+          inputProps={{ step: "1", min: 1, max: 1000, defaultValue: 100 }}
+        />
+
+        <Button
+          disabled={navigation.state === "submitting"}
+          variant="contained"
+          onClick={() => formRef.current.reset()}
+        >
+          Clear
+        </Button>
+        <Button
+          disabled={navigation.state === "submitting"}
+          variant="contained"
+          type="submit"
+        >
+          {navigation.state === "submitting" ? "Submitting" : "Query"}
+        </Button>
         <div className={classes.actions}>
-          <Button disabled={submitting} variant="contained">
-            Clear
+          <Button
+            disabled={directionsDisabled}
+            variant="contained"
+            onClick={() => formRef.current.reset()}
+          >
+            Prev
           </Button>
-          <Button disabled={submitting} variant="contained" type="submit">
-            {submitting ? "Submitting" : "Query"}
+          <Button
+            disabled={directionsDisabled}
+            variant="contained"
+            onClick={() => formRef.current.reset()}
+          >
+            Next
           </Button>
         </div>
       </Form>
